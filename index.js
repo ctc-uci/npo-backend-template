062d71de30e992f44451a6db069b094672deb03e
Zubair Sidhu <sidhuzubair@gmail.com>
2021-10-30 14:36:30 -0700

    Add express boilerplate

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+var cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(cors({
+  origin: `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`,
+}));
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
