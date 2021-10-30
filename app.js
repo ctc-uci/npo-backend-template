c29abee5c2ab81430ec26309121f597eed47ccab
Zubair Sidhu <sidhuzubair@gmail.com>
2021-10-30 15:09:09 -0700

    Add husky and lint-staged

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.use(cors({
-  origin: `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`,
-}));
+app.use(
+  cors({
+    origin: `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`,
+  }),
+);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
